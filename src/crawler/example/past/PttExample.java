4ba0cb3af940b40b41bc7b22a71848d06a604829
yijinwu <wuyijin538@gamil.com>
2017-12-10 12:10:22 +0800

    Ptt main-content  2017.12.10 12:10

--- src/crawler/example/past/PttExample.java
+++ src/crawler/example/past/PttExample.java
@@ -3,6 +3,8 @@ package crawler.example.past;
 import com.github.abola.crawler.CrawlerPack;
 import org.apache.commons.logging.impl.SimpleLog;
 
+import org.jsoup.select.Elements;
+
 /**
  * 爬蟲包程式的全貌，就只有這固定的模式
  * 
@@ -25,8 +27,8 @@ public class PttExample {
 
 		String uri = "https://www.ptt.cc/bbs/Stock/M.1512845625.A.188.html";
 
-		System.out.println(
-				CrawlerPack.start()
+	//	System.out.println(
+	//			CrawlerPack.start()
 				
 				// 參數設定
 			    //.addCookie("key","value")	// 設定cookie
@@ -34,13 +36,25 @@ public class PttExample {
 				
 				// 選擇資料格式 (三選一)
 				//.getFromJson(uri)
-			    .getFromHtml(uri)
+	//		    .getFromHtml(uri)
 			    //.getFromXml(uri)
 			    
 			    // 這兒開始是 Jsoup Document 物件操作
 			   // .select(".css .selector ")
 			   //	.select("#main-content > div:nth-child(3) > span.article-meta-value") //抓文章標題
-				.select("span.hl.push-tag:contains(推)+span.f3.hl.push-userid") //抓推文作者
-		);
+	//			.select("span.hl.push-tag:contains(推)+span.f3.hl.push-userid") //抓推文作者
+     //   );
+        Elements jsoup =  CrawlerPack.start()
+
+                // 參數設定
+                .addCookie("over18","1") 	// 設定cookie
+                //.setRemoteEncoding("big5")// 設定遠端資料文件編碼
+
+                // 選擇資料格式 (三選一)
+                .getFromHtml(uri)
+
+                // 這兒開始是 Jsoup Document 物件操作
+                .select("#main-content");
+                System.out.println(jsoup.select("span,div").remove().text());
 	}
 }
